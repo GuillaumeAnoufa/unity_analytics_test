5c4714bfd368fff201364282544a4795e1ea21c6
Guillaume Anoufa <guillaume.anoufa@airbus.com>
2023-07-26 18:43:41 +0200

    added ability to multi jump (wow), problem it resets on collide (walls included) -> create multiple tile maps for ground and walls

--- Assets/persoScript.cs
+++ Assets/persoScript.cs
@@ -7,25 +7,53 @@ using UnityEngine;
 public class persoScript : MonoBehaviour
 {
     [SerializeField] private float horizontalSpeed = 50.0f;
-    private Vector3 newPos;
+    private Rigidbody2D rb2D;
+    private Vector3 newPos = Vector3.zero;
+    private Vector2 direction = Vector2.zero;
+    private int jumpCtr = 0;
+    private int maxNbOfJump = 2;
+    [SerializeField] private Vector2 jump = new Vector2(0, 5000);
 
     // Start is called before the first frame update
     void Start()
     {
-        newPos = transform.position;
+        rb2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        direction = Vector2.zero;
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
         {
-            newPos.x += horizontalSpeed * Time.deltaTime;
+            direction.x = 1;
         }
         if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
         {
-            newPos.x -= horizontalSpeed * Time.deltaTime;
+            direction.x = -1;
         }
-        transform.position = newPos;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (jumpCtr <= maxNbOfJump)
+            {
+                rb2D.AddForce(jump, ForceMode2D.Impulse);
+                jumpCtr++;
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb2D.velocity = new Vector2(direction.x * horizontalSpeed 
+            * Time.fixedDeltaTime, rb2D.velocity.y);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.tag == "ground")
+        {
+            jumpCtr = 0;
+        }
     }
 }
